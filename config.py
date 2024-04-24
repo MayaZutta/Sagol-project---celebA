556618c988cb410899c5b84d221740f937a82d28
MayaZutta <157239003+MayaZutta@users.noreply.github.com>
2024-04-24 19:42:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 @dataclass
 class trainConfig():
-    root_dir: str = r"C:\Users\Owner\Desktop\SagolProject_Galit\CelebA" # should i chnage them to the uni comp? where?
+    root_dir: str = r"C:\Users\Owner\Desktop\SagolProject_Galit\CelebA"
     data_dir: str = r"C:\Users\Owner\Desktop\SagolProject_Galit\CelebA"
     exp_name: str  = 'exp_try_2'
     epochs: int = 100
