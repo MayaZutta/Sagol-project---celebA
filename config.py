d9020d29c5cd8a614f020c756b0c9d36569d69f3
MayaZutta <maya.zutta@gmail.com>
2024-05-02 13:23:28 +0300

    modified:   config.py  gality path

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import datetime
 
 @dataclass
 class trainConfig():
-    root_dir: str = r"C:\Users\Owner\Desktop\SagolProject_Galit\CelebA" # need to be changed in power9 to..
-    data_dir: str = r"C:\Users\Owner\Desktop\SagolProject_Galit\CelebA"
+    root_dir: str = r"/galitylab/data/celeba" 
+    data_dir: str = r"/galitylab/data/celeba"
     current_time = datetime.datetime.now()
     exp_name: str = f'exp_try_{current_time.strftime("%m%d_%H%M")}'
     epochs: int = 100
